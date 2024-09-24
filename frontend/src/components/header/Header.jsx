018bc9d96c50e715febbca1b2f231bc4af2b4474
Piyush Barapatre <82228766+piyushbarapatre@users.noreply.github.com>
2024-09-24 20:47:25 +0530

    Add files via upload

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleLg">BLOG</span>
+      </div>
+      <img
+        className="headerImg"
+        src="./images/beautiful-desert-landscape.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
