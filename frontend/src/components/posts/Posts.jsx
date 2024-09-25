6a4c290535f94babb8652725758d6a4bb8d7f36b
Piyush Barapatre <82228766+piyushbarapatre@users.noreply.github.com>
2024-09-25 12:58:43 +0530

    Add files via upload

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -1,15 +1,12 @@
-
 import Post from "../post/Post";
 import "./posts.css";
 
-export default function Posts() {
+export default function Posts({ posts }) {
   return (
     <div className="posts">
-      <Post img="./images/beautiful-mountains-landscape.jpg" />
-      <Post img="./images/futuristic-moon-background.jpg" />
-      <Post img="./images/beautiful-photorealistic-moon.jpg"/>
-      <Post img="./images/desert-with-sand-dunes.jpg"/>
-      <Post img="./images/bright-colorful-pop-landscape.jpg"/>
+      {posts.map((p) => (
+        <Post post={p} />
+      ))}
     </div>
   );
 }
